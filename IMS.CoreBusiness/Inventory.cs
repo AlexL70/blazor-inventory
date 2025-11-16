a58202f7f5848efe0a9025aaa51c09572293461c
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-11-16 13:49:44 +0200

    28. Form Validations with Data Annotations

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace IMS.CoreBusiness
 {
     public class Inventory
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
         public string Name { get; set; } = string.Empty;
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater than or equal to 0")]
         public int Quantity { get; set; }
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be greater than or equal to 0")]
         public decimal Price { get; set; }
     }
 }
