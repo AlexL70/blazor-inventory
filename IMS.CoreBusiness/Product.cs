49f679c325745b8a3d0031fcd7f5710db99bbbdb
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-11-18 12:14:29 +0200

    49. Add Inventory to Product

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -12,5 +12,31 @@ namespace IMS.CoreBusiness
         public int Quantity { get; set; }
         [Range(0, double.MaxValue, ErrorMessage = "Price must be greater than or equal to 0")]
         public decimal Price { get; set; }
+
+        public IList<ProductInventory> Inventories { get; set; } = [];
+
+        public void AddInventory(Inventory inventory)
+        {
+            var existing = Inventories
+                .FirstOrDefault(pi => pi.InventoryId == inventory.Id);
+            if (existing != null)
+                return;
+            else
+                Inventories.Add(new ProductInventory
+                {
+                    ProductId = Id,
+                    Product = this,
+                    InventoryId = inventory.Id,
+                    Inventory = inventory,
+                    Quantity = 1
+                });
+        }
+
+        public void RemoveInventory(ProductInventory productInventory)
+        {
+            if (!Inventories.Contains(productInventory))
+                return;
+            Inventories.Remove(productInventory);
+        }
     }
 }
